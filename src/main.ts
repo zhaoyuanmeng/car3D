abf644b86e5481a325fe854fa2dbc7052b6c89a2
zhaoyuanda <1426593075@qq.com>
2023-05-18 14:07:33 +0800

    feat:add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router'; // 创建vue实例
-
 const app = createApp(App);
+import '@/assets/main.css';
 
 app.use(router); // 挂载路由
 app.use(store); // 挂载pinia
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>测试 vueUse 的鼠标坐标</h1>
   <h3>Mouse: {{ x }} x {{ y }}</h3>
+  <h1 class="text-blue-600 font-bold underline">Hello world!</h1>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
