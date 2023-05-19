cb6b8f1e93b029b02be01cba56ad7b75552b6f3b
RyanHo97 <1120177906@qq.com>
2023-05-19 11:17:39 +0800

    test threejs success

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,93 @@
 <template>
-  <h1>测试 vueUse 的鼠标坐标</h1>
-  <h3>Mouse: {{ x }} x {{ y }}</h3>
-  <h1 class="text-blue-600 font-bold underline">Hello world!</h1>
+  <div class="container">
+    <h1>测试3D threejs页面</h1>
+    <div class="panel w-full" ref="canvasContainer"></div>
+  </div>
 </template>
+
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useMouse } from '@vueuse/core';
+import { defineComponent, onMounted, ref } from 'vue';
+import * as THREE from 'three';
 
 export default defineComponent({
   name: 'VueUse',
   setup() {
-    const { x, y } = useMouse();
+    const canvasContainer = ref<HTMLDivElement | null>(null);
+
+    onMounted(() => {
+      if (canvasContainer.value) {
+        // 创建场景
+        const scene = new THREE.Scene();
+
+        // 创建相机
+        const camera = new THREE.PerspectiveCamera(
+          75,
+          window.innerWidth / window.innerHeight,
+          0.1,
+          1000,
+        );
+
+        // 创建渲染器
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        canvasContainer.value.appendChild(renderer.domElement);
+
+        // 创建正方形
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        // 调整视角
+        camera.position.z = 5;
+
+        // 动画循环
+        const animate = function () {
+          requestAnimationFrame(animate);
+
+          // 旋转正方形
+          cube.rotation.x += 0.01;
+          cube.rotation.y += 0.01;
+
+          // 渲染场景和相机
+          renderer.render(scene, camera);
+        };
+
+        animate();
+      }
+    });
 
     return {
-      x,
-      y,
+      canvasContainer,
     };
   },
 });
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+h1 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  line-height: 50px;
+  z-index: 999;
+  color: #ffffff;
+}
+
+.panel {
+  width: 100%;
+  height: 100%;
+}
+</style>
