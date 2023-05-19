b66ee2267b67d0d27d863eb43a699ad9939cb440
RyanHo97 <1120177906@qq.com>
2023-05-19 10:50:44 +0800

    packaging optimization

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,33 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['axios', 'lodash'],
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        unused: true,
+        dead_code: true,
+        passes: 3,
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      output: {
+        generatedCode: 'es2015',
+        manualChunks: {
+          vendor: ['vue', 'vue-router', 'pinia', 'axios'],
+          three: ['three'],
+        },
+        compact: true,
+        sanitizeFileName: true,
+      },
+    },
+  },
 });
